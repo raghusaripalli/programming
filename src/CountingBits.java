8f313c464688613d0293bf63cdbabcbb20877bca
raghusaripalli <rvs1604@gmail.com>
2020-05-28 18:06:33 +0530

    Day 28

---
+++ src/CountingBits.java
@@ -0,0 +1,34 @@
+public class CountingBits {
+    public int[] countBits(int num) {
+        int[] res = new int[++num];
+        int i = 0, startIdx = 2, endIdx = 4, j = 4;
+        while (i < num && i <= 4) {
+            if (i == 0) res[0] = 0;
+            else if (i == 1) res[1] = 1;
+            else if (i == 2) res[2] = 1;
+            else if (i == 3) res[3] = 2;
+            i++;
+        }
+        while (num > 4 && j < num) {
+            i = startIdx;
+            while (i < endIdx && j < num) {
+                res[j++] = res[i++];
+            }
+            i = startIdx;
+            while (i < endIdx && j < num) {
+                res[j++] = res[i++] + 1;
+            }
+            startIdx *= 2;
+            endIdx *= 2;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        CountingBits countingBits = new CountingBits();
+        int[] res = countingBits.countBits(31);
+        for (int re : res) {
+            System.out.print(re + " ");
+        }
+    }
+}
