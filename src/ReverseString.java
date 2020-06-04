4827796491c497ea36fea96c46c8c3de055589df
raghusaripalli <rvs1604@gmail.com>
2020-06-04 13:43:02 +0530

    Day 35

---
+++ src/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        char a, b;
+        for (int i = 0; i < n / 2; i++) {
+            a = s[i];
+            b = s[n-i-1];
+            if (a!=b) {
+                s[i] = b;
+                s[n-i-1] = a;
+            }
+        }
+    }
+}
