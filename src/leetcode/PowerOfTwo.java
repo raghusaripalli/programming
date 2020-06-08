0d8b2f4397479fd0afc5e5e2abb719f60b891ea2
raghusaripalli <rvs1604@gmail.com>
2020-06-08 22:46:59 +0530

    Day 39

---
+++ src/leetcode/PowerOfTwo.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && ((n & (n - 1)) == 0);
+    }
+
+    public static void main(String[] args) {
+        PowerOfTwo two = new PowerOfTwo();
+        System.out.println(two.isPowerOfTwo(Integer.MIN_VALUE));
+        System.out.println(two.isPowerOfTwo(-2));
+        System.out.println(two.isPowerOfTwo(-1));
+        System.out.println(two.isPowerOfTwo(0));
+        System.out.println(two.isPowerOfTwo(1));
+        System.out.println(two.isPowerOfTwo(16));
+        System.out.println(two.isPowerOfTwo(218));
+        System.out.println(two.isPowerOfTwo(Integer.MAX_VALUE));
+    }
+}
