9927181c690bc9978ffc130df41caecbfded30da
raghusaripalli <rvs1604@gmail.com>
2020-06-13 17:10:33 +0530

    Day 44

---
+++ src/leetcode/LargestDivisibleSubset.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestDivisibleSubset {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        int[] count = new int[n];
+        int[] pre = new int[n];
+        Arrays.sort(nums);
+        int max = 0, index = -1;
+        for (int i = 0; i < n; i++) {
+            count[i] = 1;
+            pre[i] = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] % nums[j] == 0) {
+                    if (1 + count[j] > count[i]) {
+                        count[i] = count[j] + 1;
+                        pre[i] = j;
+                    }
+                }
+            }
+            if (count[i] > max) {
+                max = count[i];
+                index = i;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while (index != -1) {
+            res.add(nums[index]);
+            index = pre[index];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LargestDivisibleSubset subset = new LargestDivisibleSubset();
+        int[] nums = new int[3];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        List<Integer> res = subset.largestDivisibleSubset(nums);
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+    }
+}
