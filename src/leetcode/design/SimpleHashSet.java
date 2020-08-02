f39d02dec4f3ee8805ac57e8544f4f614a242f9b
raghusaripalli <rvs1604@gmail.com>
2020-08-02 15:21:57 +0530

    Design Problems - Hash set

---
+++ src/leetcode/design/SimpleHashSet.java
@@ -0,0 +1,89 @@
+package leetcode.design;
+
+public class SimpleHashSet {
+
+    class Node {
+        int key;
+        Node next;
+
+        Node(int key) {
+            this.key = key;
+        }
+    }
+
+    private Node[] arr;
+    private static final int size = 10011;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public SimpleHashSet() {
+        arr = new Node[size];
+    }
+
+    public void add(int key) {
+        int i = myHash(key);
+        if (arr[i] == null) {
+            arr[i] = new Node(key);
+        } else {
+            Node cur = arr[i];
+            while (cur.next != null && cur.key != key) {
+                cur = cur.next;
+            }
+            if (cur.key != key) {
+                cur.next = new Node(key);
+            }
+        }
+    }
+
+    public void remove(int key) {
+        int i = myHash(key);
+        if (arr[i] == null) {
+            return;
+        } else if (arr[i].key == key) {
+            arr[i] = arr[i].next;
+        } else {
+            Node cur = arr[i], pre = null;
+            while (cur.next != null && cur.key != key) {
+                pre = cur;
+                cur = cur.next;
+            }
+            if (pre != null && cur.key == key) {
+                pre.next = cur.next;
+            }
+        }
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        int i = myHash(key);
+        if (arr != null) {
+            Node cur = arr[i];
+            while (cur != null) {
+                if (cur.key == key) {
+                    return true;
+                }
+                cur = cur.next;
+            }
+        }
+        return false;
+    }
+
+    private int myHash(int key) {
+        return key % size;
+    }
+
+    public static void main(String[] args) {
+        SimpleHashSet hashSet = new SimpleHashSet();
+        hashSet.add(1);
+        hashSet.add(2);
+        System.out.println(hashSet.contains(1));
+        System.out.println(hashSet.contains(3));
+        hashSet.add(2);
+        System.out.println(hashSet.contains(2));
+        hashSet.remove(2);
+        System.out.println(hashSet.contains(2));
+    }
+}
