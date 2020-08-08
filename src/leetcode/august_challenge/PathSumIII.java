58a7cddb7b75b2e45bb990b42bc9758a2c5f8677
raghusaripalli <rvs1604@gmail.com>
2020-08-08 17:00:21 +0530

    Path Sum in a Tree

---
+++ src/leetcode/august_challenge/PathSumIII.java
@@ -0,0 +1,43 @@
+package leetcode.august_challenge;
+
+import java.util.HashMap;
+
+public class PathSumIII {
+    private int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        return helper(root, 0, sum, preSum);
+    }
+
+    private int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        currSum += root.val;
+        int res = preSum.getOrDefault(currSum - target, 0);
+        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+
+        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        PathSumIII pathSumIII = new PathSumIII();
+
+        // test Case 1
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(-3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(2);
+        root.right.right = new TreeNode(11);
+        root.left.left.left = new TreeNode(3);
+        root.left.left.right = new TreeNode(-2);
+        root.left.left.right = new TreeNode(1);
+
+        System.out.println(pathSumIII.pathSum(root, 8));
+        System.out.println(pathSumIII.pathSum(root, 9));
+    }
+}
