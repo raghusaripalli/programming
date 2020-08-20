1a6d9531471484aa08baa5932a172032bcd53ca8
raghusaripalli <rvs1604@gmail.com>
2020-08-20 23:44:31 +0530

    Fix MergeSort

--- src/leetcode/sorting/MergeSort.java
+++ src/leetcode/sorting/MergeSort.java
@@ -3,10 +3,10 @@ package leetcode.sorting;
 public class MergeSort {
 
     private void merge(int[] arr, int lo, int mid, int hi) {
-        int len = hi - lo;
+        int len = hi - lo + 1;
         int[] aux = new int[len];
-        int k = 0, i = lo, j = mid;
-        while (i < mid && j < hi) {
+        int k = 0, i = lo, j = mid + 1;
+        while (i <= mid && j <= hi) {
             if (arr[i] <= arr[j]) {
                 aux[k++] = arr[i++];
             } else {
@@ -14,23 +14,23 @@ public class MergeSort {
             }
         }
 
-        while (i < mid) {
+        while (i <= mid) {
             aux[k++] = arr[i++];
         }
 
-        while (j < hi) {
+        while (j <= hi) {
             aux[k++] = arr[j++];
         }
 
         k = 0;
-        while (lo < hi) {
+        while (lo <= hi) {
             arr[lo++] = aux[k++];
         }
     }
 
     private void mergeSort(int[] arr, int lo, int hi) {
         if (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
+            int mid = (lo + hi) / 2;
             mergeSort(arr, lo, mid);
             mergeSort(arr, mid + 1, hi);
             merge(arr, lo, mid, hi);
@@ -39,13 +39,13 @@ public class MergeSort {
 
     public void sort(int[] arr) {
         int lo = 0;
-        int hi = arr.length;
+        int hi = arr.length - 1;
         mergeSort(arr, lo, hi);
     }
 
     public static void main(String[] args) {
         MergeSort ms = new MergeSort();
-        int[] data = new int[]{12, 11, 13, 5, 6, 7};
+        int[] data = new int[]{12, -2, 11, 13, -1, 5, 6, 7, 2, 1 ,6, -2};
         ms.sort(data);
         for (int i : data) {
             System.out.print(i + " ");
