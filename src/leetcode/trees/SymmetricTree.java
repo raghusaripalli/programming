43ecbef2e49e66fbf29ffd963532aae6788cf126
raghusaripalli <rvs1604@gmail.com>
2020-09-22 23:15:37 +0530

    Symmetric Tree

--- src/leetcode/trees/SymmetricTree.java
+++ src/leetcode/trees/SymmetricTree.java
@@ -2,9 +2,30 @@ package leetcode.trees;
 
 import leetcode.august_challenge.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SymmetricTree {
-    private boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetricRecursion(TreeNode root) {
         return isMirror(root, root);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
     }
 
     private boolean isMirror(TreeNode t1, TreeNode t2) {
@@ -27,5 +48,6 @@ public class SymmetricTree {
         root.right.right = new TreeNode(3);
 
         System.out.println(symmetricTree.isSymmetric(root));
+        System.out.println(symmetricTree.isSymmetricRecursion(root));
     }
 }
